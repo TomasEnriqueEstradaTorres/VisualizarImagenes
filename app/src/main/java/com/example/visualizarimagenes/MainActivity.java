02f9c0c8b28648450e6a26141f68c57459fee38f
Tomas Enrique <tomasenriquea80@gmail.com>
2019-01-26 16:15:26 +0100

    rotacion de imagnes y termino - OK
    Se ha hecho que la imagen de un boton de opcion rote cada vez que se le hace click.
    
    Tambien se ha agregado un metodo para comprobar si la imagen de las opciones es identica a la imagen ganadora.
    
    Funcionamiento aceptable, falta pulir detalles, por lo demas OK.

--- app/src/main/java/com/example/visualizarimagenes/MainActivity.java
+++ app/src/main/java/com/example/visualizarimagenes/MainActivity.java
@@ -17,13 +17,15 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     private ImageView reloj, opcion1, opcion2, opcion3, opcion4, referencia; //opciones de juego
     private ImageView inter1, inter2; // imagenes pequenyas en movimiento
     private AnimationDrawable  animacionReloj, animacion1, animacion2; //animacion por fotograma
     private Animation anim1, anim2; // servira para la animacion por interpolacion
-    private Bitmap referenciaBitmap, bitmap1, bitmap2, bitmap3, bitmap4, pruebaBBBB; // para obtener referencia de comparacion de imagenes
+    private Bitmap referenciaBitmap, bitmap1, bitmap2, bitmap3, bitmap4; // para obtener referencia de comparacion de imagenes
     private TextView mensaje;  //mostrara el mensaje de aviso si se gana o pierde
     private Button reinicioJuego; // boton para reinicioar el juego
     private final String mensajeGanador = "¡¡¡Haz Acertado Enhorabuena!!!";
@@ -31,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
     private final String mensajeFinJuego = "¡¡¡FIN DE JUEGO!!!";
     private final String mensajeError = "¡¡¡FAIL!!!";
     private final String mensajeReinicio= "JUEGO REINICIADO";
+    private static final Random rgenerador = new Random();//servira par rotar las imagenes de las opciones
+    private static final Integer[] imagenesID = {R.drawable.aventurero12, R.drawable.sombiea18, R.drawable.sombieb24};//lista de imagenes
+    private int resource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,6 +74,8 @@ public class MainActivity extends AppCompatActivity {
         //Servira para poder realizar la comparacion de imagenes para saber si acertamos.
         referencia.setVisibility(View.INVISIBLE);
         referenciaBitmap = ((BitmapDrawable)referencia.getDrawable()).getBitmap();
+        //Se encargara de rotal las imagenes de la opciones
+        Integer q = imagenesID[rgenerador.nextInt(imagenesID.length)];
     }
 
     @Override
@@ -79,20 +86,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void onResume() {
         super.onResume();
-
+        //Todo esto seran los botones en la pantalla
         opcion1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                opcion1.setImageResource(R.drawable.aventurero12);
-                bitmap1 = ((BitmapDrawable)opcion1.getDrawable()).getBitmap();
-                comprobarImagen(referenciaBitmap, bitmap1);
+                resource = imagenesID[rgenerador.nextInt(imagenesID.length)]; // rota la imagen cada vez que se hace un click
+                opcion1.setImageResource(resource);// recibe el resultado del ramdon de imagenes
+                bitmap1 = ((BitmapDrawable)opcion1.getDrawable()).getBitmap();//obtiene la referencia 'bitmap' de la imagen
+                comprobarImagen(referenciaBitmap, bitmap1); //comprueba si las imagenes son iguales
             }
         });
 
         opcion2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                opcion2.setImageResource(R.drawable.sombiea18);
+                resource = imagenesID[rgenerador.nextInt(imagenesID.length)];
+                opcion2.setImageResource(resource);
                 bitmap2 = ((BitmapDrawable)opcion2.getDrawable()).getBitmap();
                 comprobarImagen(referenciaBitmap, bitmap2);
             }
@@ -101,7 +110,8 @@ public class MainActivity extends AppCompatActivity {
         opcion3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                opcion3.setImageResource(R.drawable.sombieb24);
+                resource = imagenesID[rgenerador.nextInt(imagenesID.length)];
+                opcion3.setImageResource(resource);
                 bitmap3 = ((BitmapDrawable)opcion3.getDrawable()).getBitmap();
                 comprobarImagen(referenciaBitmap, bitmap3);
             }
@@ -110,12 +120,12 @@ public class MainActivity extends AppCompatActivity {
         opcion4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                opcion4.setImageResource(R.drawable.sombiea18);
+                resource = imagenesID[rgenerador.nextInt(imagenesID.length)];
+                opcion4.setImageResource(resource);
                 bitmap4 = ((BitmapDrawable)opcion4.getDrawable()).getBitmap();
                 comprobarImagen(referenciaBitmap, bitmap4);
             }
         });
-
 
         reinicioJuego.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -124,6 +134,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 
     public void onRestart() {
         super.onRestart();
@@ -153,19 +164,25 @@ public class MainActivity extends AppCompatActivity {
         opcion3.setImageResource(R.drawable.regalo);
         opcion4.setImageResource(R.drawable.regalo);
         //hace visible las otras imagenes
+        opcion1.setVisibility(View.VISIBLE);
         opcion2.setVisibility(View.VISIBLE);
         opcion3.setVisibility(View.VISIBLE);
         opcion4.setVisibility(View.VISIBLE);
         //oculta el boton de reinicio
         reinicioJuego.setVisibility(View.INVISIBLE);
-        toastPersonalizado(mensajeReinicio);
+        toastPersonalizado(mensajeReinicio);//muestra un mensaje de reinicio de juego
         //iniciara las animaciones
         animacion1.start();
         animacion2.start();
         animacionReloj.start();
         inter1.startAnimation(anim1);
         inter2.startAnimation(anim2);
-        mensaje.setText(R.string.adivida); // reinicia el mensaje del juego
+        mensaje.setText(R.string.adivina); // reinicia el mensaje del juego
+        //reactivara los botones imagenes de las obciones
+        opcion1.setEnabled(true);
+        opcion2.setEnabled(true);
+        opcion3.setEnabled(true);
+        opcion4.setEnabled(true);
     }
 
     //Funcion para comprobar si la imagen elegida es la correcta
@@ -175,9 +192,30 @@ public class MainActivity extends AppCompatActivity {
             mensaje.setTextColor(Color.BLUE);
             toastPersonalizado(mensajeFinJuego);
             //ocultara las imagenes si se acierta y gana
-            opcion2.setVisibility(View.INVISIBLE);
-            opcion3.setVisibility(View.INVISIBLE);
-            opcion4.setVisibility(View.INVISIBLE);
+            if (referenciaBitmap.equals(bitmap1)){
+                opcion2.setVisibility(View.INVISIBLE);
+                opcion3.setVisibility(View.INVISIBLE);
+                opcion4.setVisibility(View.INVISIBLE);
+                opcion1.setEnabled(false); // desactivara el boton imagen para que no cambie de nuevo
+            }
+            else if (referenciaBitmap.equals(bitmap2)){
+                opcion1.setVisibility(View.INVISIBLE);
+                opcion3.setVisibility(View.INVISIBLE);
+                opcion4.setVisibility(View.INVISIBLE);
+                opcion2.setEnabled(false);// desactivara el boton imagen para que no cambie de nuevo
+            }
+            else if (referenciaBitmap.equals(bitmap3)){
+                opcion1.setVisibility(View.INVISIBLE);
+                opcion2.setVisibility(View.INVISIBLE);
+                opcion4.setVisibility(View.INVISIBLE);
+                opcion3.setEnabled(false);// desactivara el boton imagen para que no cambie de nuevo
+            }
+            else if (referenciaBitmap.equals(bitmap4)){
+                opcion1.setVisibility(View.INVISIBLE);
+                opcion2.setVisibility(View.INVISIBLE);
+                opcion3.setVisibility(View.INVISIBLE);
+                opcion4.setEnabled(false);// desactivara el boton imagen para que no cambie de nuevo
+            }
             reinicioJuego.setVisibility(View.VISIBLE); //mostrara el boton de reinicio
             //detendra las animaciones
             animacion1.stop();
@@ -198,10 +236,14 @@ public class MainActivity extends AppCompatActivity {
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             public void run() {
-                opcion2.setImageResource(R.drawable.regalo);
-                opcion3.setImageResource(R.drawable.regalo);
-                opcion4.setImageResource(R.drawable.regalo);
-                mensaje.setText(R.string.adivida); // reinicia el mensaje
+                //esto comprueba si la imagen es la ganadora y si lo es no retornara a la imagen por defecto de inicio
+                if (referenciaBitmap != bitmap1 || referenciaBitmap != bitmap2 || referenciaBitmap != bitmap3 || referenciaBitmap != bitmap4){
+                    opcion1.setImageResource(R.drawable.regalo);
+                    opcion2.setImageResource(R.drawable.regalo);
+                    opcion3.setImageResource(R.drawable.regalo);
+                    opcion4.setImageResource(R.drawable.regalo);
+                }
+                mensaje.setText(R.string.adivina); // reinicia el mensaje
                 mensaje.setTextColor(Color.BLUE); //reinicia el color de texto
             }
         }, 2000); // retardo de dos segundos
@@ -218,7 +260,7 @@ public class MainActivity extends AppCompatActivity {
         //tenemos acceso a cualquier widget del layout del Toast
         TextView textView = (TextView) toast_layout.findViewById(R.id.mensajeToast);
         textView.setText(mensaje);
-        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setDuration(Toast.LENGTH_SHORT);
         toast.show();
     }
 
