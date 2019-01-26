024044cb61950853a0adc9111bf08d43d3dcb866
Tomas Enrique <tomasenriquea80@gmail.com>
2019-01-26 07:14:39 +0100

    animacion por interpolacion y otros
    se ha agregado unas imagenes con animacion por interpolacion, tambien el retorno a la imagen por defecto de las opciones, reinicio del juego.
    
    Se ha creado un Toast personalizado para una mejor visualizacion del juego.
    
    Se ha hecho que las animaciones paren cuando se gana el juego y que aparezca un boton de reinicio.

--- app/src/main/java/com/example/visualizarimagenes/MainActivity.java
+++ app/src/main/java/com/example/visualizarimagenes/MainActivity.java
@@ -1,51 +1,225 @@
 package com.example.visualizarimagenes;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView reloj;
-    private ImageView opcion1;
-    private ImageView opcion2;
-    private ImageView opcion3;
-    private ImageView opcion4;
+    private ImageView reloj, opcion1, opcion2, opcion3, opcion4, referencia; //opciones de juego
+    private ImageView inter1, inter2; // imagenes pequenyas en movimiento
+    private AnimationDrawable  animacionReloj, animacion1, animacion2; //animacion por fotograma
+    private Animation anim1, anim2; // servira para la animacion por interpolacion
+    private Bitmap referenciaBitmap, bitmap1, bitmap2, bitmap3, bitmap4, pruebaBBBB; // para obtener referencia de comparacion de imagenes
+    private TextView mensaje;  //mostrara el mensaje de aviso si se gana o pierde
+    private Button reinicioJuego; // boton para reinicioar el juego
+    private final String mensajeGanador = "¡¡¡Haz Acertado Enhorabuena!!!";
+    private final String mensajePerdedor = "¡¡¡Que Pena, sigue intentando!!!";
+    private final String mensajeFinJuego = "¡¡¡FIN DE JUEGO!!!";
+    private final String mensajeError = "¡¡¡FAIL!!!";
+    private final String mensajeReinicio= "JUEGO REINICIADO";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //sera para la animacion del reloj
         reloj = findViewById(R.id.imageViewReloj);
+        //Seran las opciones de juego
         opcion1 = findViewById(R.id.imageViewOpcion1);
         opcion2 = findViewById(R.id.imageViewOpcion2);
         opcion3 = findViewById(R.id.imageViewOpcion3);
         opcion4 = findViewById(R.id.imageViewOpcion4);
+        referencia = findViewById(R.id.imageViewReferencia);
+        //seran las pequenas animaciones por fotograma e interpolacion
+        inter1 = findViewById(R.id.imageViewInter1);
+        inter2 = findViewById(R.id.imageViewInter2);
+        //mostrara el mensaje si acertaste al elejir
+        mensaje = findViewById(R.id.textViewMensaje);
+        //Boton para reiniciar el juego
+        reinicioJuego = findViewById(R.id.buttonReinicio);
 
 
-        AnimationDrawable animacionReloj = (AnimationDrawable)reloj.getDrawable();
+        //imagenes de sombie vigilante femenino
+        animacion1 = (AnimationDrawable)inter1.getDrawable();// movimiento por fotograma
+        animacion1.start();
+        anim1 = AnimationUtils.loadAnimation(this, R.anim.caminar); //movimiento por interpolacion
+        inter1.startAnimation(anim1);
+        //imagenes de sombie vigilante masculino
+        animacion2 = (AnimationDrawable)inter2.getDrawable();// movimiento por fotograma
+        animacion2.start();
+        anim2 = AnimationUtils.loadAnimation(this, R.anim.caminar); //movimiento por interpolacion
+        inter2.startAnimation(anim2);
+        //Esto es la animacion del reloj por medio de fotogramas
+        animacionReloj = (AnimationDrawable)reloj.getDrawable();
         animacionReloj.start();
+        //Servira para poder realizar la comparacion de imagenes para saber si acertamos.
+        referencia.setVisibility(View.INVISIBLE);
+        referenciaBitmap = ((BitmapDrawable)referencia.getDrawable()).getBitmap();
+    }
 
-        AnimationDrawable animacionOpcion1 = (AnimationDrawable)opcion1.getDrawable();
-        animacionOpcion1.start();
-
-        AnimationDrawable animacionOpcion2 = (AnimationDrawable)opcion2.getDrawable();
-        animacionOpcion2.start();
-
-        AnimationDrawable animacionOpcion3 = (AnimationDrawable)opcion3.getDrawable();
-        animacionOpcion3.start();
-
-        AnimationDrawable animacionOpcion4 = (AnimationDrawable)opcion4.getDrawable();
-        animacionOpcion4.start();
-
-
-
-
-
-
-
+    @Override
+    public void onStart() {
+        super.onStart();
 
     }
+
+    public void onResume() {
+        super.onResume();
+
+        opcion1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                opcion1.setImageResource(R.drawable.aventurero12);
+                bitmap1 = ((BitmapDrawable)opcion1.getDrawable()).getBitmap();
+                comprobarImagen(referenciaBitmap, bitmap1);
+            }
+        });
+
+        opcion2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                opcion2.setImageResource(R.drawable.sombiea18);
+                bitmap2 = ((BitmapDrawable)opcion2.getDrawable()).getBitmap();
+                comprobarImagen(referenciaBitmap, bitmap2);
+            }
+        });
+
+        opcion3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                opcion3.setImageResource(R.drawable.sombieb24);
+                bitmap3 = ((BitmapDrawable)opcion3.getDrawable()).getBitmap();
+                comprobarImagen(referenciaBitmap, bitmap3);
+            }
+        });
+
+        opcion4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                opcion4.setImageResource(R.drawable.sombiea18);
+                bitmap4 = ((BitmapDrawable)opcion4.getDrawable()).getBitmap();
+                comprobarImagen(referenciaBitmap, bitmap4);
+            }
+        });
+
+
+        reinicioJuego.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reiniciarJuego();
+            }
+        });
+    }
+
+    public void onRestart() {
+        super.onRestart();
+
+    }
+
+    public void onPause() {
+        super.onPause();
+
+    }
+
+    public void onStop() {
+        super.onStop();
+
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+
+    }
+
+    //funcion para reiniciar el juego
+    public void reiniciarJuego(){
+        //Retorna a la imagen por defecto de inicio
+        opcion1.setImageResource(R.drawable.regalo);
+        opcion2.setImageResource(R.drawable.regalo);
+        opcion3.setImageResource(R.drawable.regalo);
+        opcion4.setImageResource(R.drawable.regalo);
+        //hace visible las otras imagenes
+        opcion2.setVisibility(View.VISIBLE);
+        opcion3.setVisibility(View.VISIBLE);
+        opcion4.setVisibility(View.VISIBLE);
+        //oculta el boton de reinicio
+        reinicioJuego.setVisibility(View.INVISIBLE);
+        toastPersonalizado(mensajeReinicio);
+        //iniciara las animaciones
+        animacion1.start();
+        animacion2.start();
+        animacionReloj.start();
+        inter1.startAnimation(anim1);
+        inter2.startAnimation(anim2);
+        mensaje.setText(R.string.adivida); // reinicia el mensaje del juego
+    }
+
+    //Funcion para comprobar si la imagen elegida es la correcta
+    public void comprobarImagen(Bitmap imagenReferencia, Bitmap imagenAcomparar){
+        if(imagenReferencia.equals(imagenAcomparar)){
+            mensaje.setText(mensajeGanador);
+            mensaje.setTextColor(Color.BLUE);
+            toastPersonalizado(mensajeFinJuego);
+            //ocultara las imagenes si se acierta y gana
+            opcion2.setVisibility(View.INVISIBLE);
+            opcion3.setVisibility(View.INVISIBLE);
+            opcion4.setVisibility(View.INVISIBLE);
+            reinicioJuego.setVisibility(View.VISIBLE); //mostrara el boton de reinicio
+            //detendra las animaciones
+            animacion1.stop();
+            animacion2.stop();
+            animacionReloj.stop();
+            inter1.clearAnimation();
+            inter2.clearAnimation();
+        }else{
+            mensaje.setText(mensajePerdedor);
+            mensaje.setTextColor(Color.RED);
+            toastPersonalizado(mensajeError);
+            retornarImagen();
+        }
+    }
+
+    //funcion para retornar a la anterior imagen si no se acierta en el juego
+    public void retornarImagen(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                opcion2.setImageResource(R.drawable.regalo);
+                opcion3.setImageResource(R.drawable.regalo);
+                opcion4.setImageResource(R.drawable.regalo);
+                mensaje.setText(R.string.adivida); // reinicia el mensaje
+                mensaje.setTextColor(Color.BLUE); //reinicia el color de texto
+            }
+        }, 2000); // retardo de dos segundos
+    }
+
+    //Funcion que personaliza el mensaje del toast
+    public void toastPersonalizado(String mensaje){
+        Toast toast = new Toast(this);
+        //usamos cualquier layout como Toast
+        View toast_layout = getLayoutInflater().inflate(R.layout.toast_personalizado, (ViewGroup) findViewById(R.id.lytLayout));
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.setView(toast_layout);
+        //se podría definir el texto en el layout si es invariable pero lo hacemos programáticamente
+        //tenemos acceso a cualquier widget del layout del Toast
+        TextView textView = (TextView) toast_layout.findViewById(R.id.mensajeToast);
+        textView.setText(mensaje);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.show();
+    }
+
 }
